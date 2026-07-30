ee41e5bd7ff48d9dc1c185ecceb48732b12c2563
Ajinkya Narendra Mahajan <71584227+ajinkyamahajan02@users.noreply.github.com>
2026-07-30 18:49:34 +0530

    Update Digit classifier (Training 4/4)

--- foundations/digit_classifier.py
+++ foundations/digit_classifier.py
@@ -6,18 +6,18 @@ class Solution(nn.Module):
     def __init__(self):
         super().__init__()
         torch.manual_seed(0)
-        self.l1 = nn.Linear(784, 512)
+        self.linear = nn.Linear(784, 512)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.2)
         self.l2 = nn.Linear(512, 10)
         self.sigmoid = nn.Sigmoid()
-        
+
     def forward(self, images: TensorType[float]) -> TensorType[float]:
         torch.manual_seed(0)
-
-        x = self.l1(images)
+        x = self.linear(images)
         x = self.relu(x)
         x = self.dropout(x)
         x = self.l2(x)
         x = self.sigmoid(x)
+
         return torch.round(x, decimals=4)
